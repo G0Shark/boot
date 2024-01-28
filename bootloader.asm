8f3272977670651550d5e564273bf68b445f0f0b
G0Shark_ <omegadroid2.0@gmail.com>
2024-01-28 21:00:49 +0700

    start git

---
+++ bootloader.asm
@@ -0,0 +1,12 @@
+; All files, idk but i save all msgs in other file. I think it will be usefull in future
+
+[org 0x7c00]
+
+KERNEL_OFFSET equ 0x1000
+
+mov [BOOT_DRIVE], dl
+
+mov bp, 0x9000
+mov sp, bp
+
+mov bx, 
